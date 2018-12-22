f3cfdabec35b42e6084c02717ca38c911191f9d6
Katsiaryna Stalchanka <e.stalchanka@gmail.com>
2018-12-23 00:23:27 +0300

    Add Level 16 task1611

--- JavaRush/src/main/tasks/task016/task1611/Solution.java
+++ JavaRush/src/main/tasks/task016/task1611/Solution.java
@@ -5,4 +5,38 @@ package main.tasks.task016.task1611;
  * @since 22-Dec-18
  */
 public class Solution {
+    public static volatile int numSeconds = 3;
+
+    public static void main(String[] args) throws InterruptedException {
+        RacingClock clock = new RacingClock();
+        //add your code here - добавь код тут
+        Thread.sleep(3500);
+        clock.interrupt();
+    }
+
+    public static class RacingClock extends Thread {
+        public RacingClock() {
+            start();
+        }
+
+        public void run() {
+            //add your code here - добавь код тут
+            final int number = numSeconds;
+            try {
+                while (numSeconds > 0) {
+                    System.out.print(numSeconds + " ");
+                    Thread.sleep(1000);
+                    numSeconds--;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (number == 3) {
+                System.out.print("Марш!");
+            } else if (number == 4) {
+                System.out.print("Прервано!");
+            }
+        }
+    }
 }
+
