0c6850d7ebf2e83874e31f2313a403d7d1f35c8e
Katsiaryna Stalchanka <e.stalchanka@gmail.com>
2018-12-22 23:59:07 +0300

    Add task1610

---
+++ JavaRush/src/main/tasks/task016/task1610/Solution.java
@@ -0,0 +1,46 @@
+package main.tasks.task016.task1610;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author Katsiaryna Stalchanka
+ * @since 22-Dec-18
+ */
+
+/*
+Считаем секунды
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(in);
+        //create and run thread
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.start();
+        //read a string
+        reader.readLine();
+        stopwatch.interrupt();
+        //close streams
+        reader.close();
+        in.close();
+    }
+
+    public static class Stopwatch extends Thread {
+        private int seconds;
+
+        public void run() {
+            try {
+                //add your code here - добавьте код тут
+                while (!currentThread().isInterrupted()) {
+                    Thread.sleep(1000);
+                    seconds += 1;
+                }
+            } catch (InterruptedException e) {
+                System.out.println(seconds);
+            }
+        }
+    }
+}
