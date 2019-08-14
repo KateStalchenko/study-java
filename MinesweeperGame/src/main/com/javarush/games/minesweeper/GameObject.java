76ff160c48135ef48dbb28367b4a2b42a36576f2
Katsiaryna Stalchanka <e.stalchanka@gmail.com>
2019-08-14 21:23:13 +0300

    Change level of access of fields and methods

--- MinesweeperGame/src/main/com/javarush/games/minesweeper/GameObject.java
+++ MinesweeperGame/src/main/com/javarush/games/minesweeper/GameObject.java
@@ -1,16 +1,16 @@
 package main.com.javarush.games.minesweeper;
 
-public class GameObject {
-    public int x;
-    public int y;
+class GameObject {
+    int x;
+    int y;
 
-    public int countMineNeighbors;
+    int countMineNeighbors;
 
-    public boolean isMine;
-    public boolean isOpen;
-    public boolean isFlag;
+    boolean isMine;
+    boolean isOpen;
+    boolean isFlag;
 
-    public GameObject(int x, int y, boolean isMine) {
+    GameObject(int x, int y, boolean isMine) {
         this.x = x;
         this.y = y;
         this.isMine = isMine;
